6dd6f2837b27d0bfdb5b377346c08f814b71a1ea
DanielFGray <DanielFGray@gmail.com>
2017-05-03 00:40:49 -0500

    fixes and tweaks

--- main.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd "${BASH_SOURCE%/*}" || exit
-source "$PWD/utils.sh"
-
-tmux bind-key "$(get_tmux_option '@fzfurls-key' 'C-u')" \; run "$PWD/match.sh"
--- open.sh
+++ open.sh
@@ -3,6 +3,10 @@
 cd "${BASH_SOURCE%/*}" || exit
 source "$PWD/utils.sh"
 
-url=$(fzf --tac --bind='Ctrl-y:execute:xclip -r <<< {}; tmux display-message "yanked"' < /tmp/tmux-fzfurls)
+url=$(fzf --tac --bind='Ctrl-y:execute:xclip -r <<< {}; tmux display-message "yanked"' $(get_tmux_option '@fzfurls-fzf-opts' '') < /tmp/tmux-fzfurls)
+
 echo > /tmp/tmux-fzfurls
+
+[[ -n "$url" ]] || exit
+
 "$(get_tmux_option '@fzfurls-cmd' 'xdg-open')" "$url"
